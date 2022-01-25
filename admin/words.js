fa7724173b50e4e321b38161a195d09946ac622c
Erdnuss3003 <61165725+Erdnuss3003@users.noreply.github.com>
2022-01-26 00:19:45 +0100

    Add files via upload

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'webersystems adapter settings': {
+        'en': 'Adapter settings for webersystems',
+        'de': 'Adaptereinstellungen für webersystems',
+        'ru': 'Настройки адаптера для webersystems',
+        'pt': 'Configurações do adaptador para webersystems',
+        'nl': 'Adapterinstellingen voor webersystems',
+        'fr': "Paramètres d'adaptateur pour webersystems",
+        'it': "Impostazioni dell'adattatore per webersystems",
+        'es': 'Ajustes del adaptador para webersystems',
+        'pl': 'Ustawienia adaptera dla webersystems',
+        'zh-cn': 'webersystems的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'Option 1',
+        'ru': 'Опция 1',
+        'pt': 'Opção 1',
+        'nl': 'Optie 1',
+        'fr': 'Option 1',
+        'it': 'opzione 1',
+        'es': 'Opción 1',
+        'pl': 'opcja 1',
+        'zh-cn': '选项1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'Option 2',
+        'ru': 'вариант 2',
+        'pt': 'opção 2',
+        'nl': 'Optie 2',
+        'fr': 'Option 2',
+        'it': 'opzione 2',
+        'es': 'opcion 2',
+        'pl': 'Opcja 2',
+        'zh-cn': '选项2'
+    }
+};
