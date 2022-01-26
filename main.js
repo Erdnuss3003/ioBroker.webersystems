3e0edceb60c6837cba62a2a5bb604b7c54ee3e67
Erdnuss3003 <christophweber2@gmx.de>
2022-01-26 23:13:43 +0100

    Update main.js

--- main.js
+++ main.js
@@ -101,7 +101,7 @@ function startAdapter(options) {
 
 async function main() {
 	var oids = ["1.3.6.1.2.1.1.5.0", "1.3.6.1.2.1.1.6.0"];
-	oidss[0]= oids[0].replaceAll(".", "_");
+	var oidss[0]= oids[0].replaceAll(".", "_");
     await adapter.setObjectNotExistsAsync(oidss[0], {
         type: 'state',
         common: {name: 'sysname', type: 'string', role: 'value', read: true, write: true},
