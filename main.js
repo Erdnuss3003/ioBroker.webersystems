a573a9afce9880696080032845b0fa500900aff2
Erdnuss3003 <christophweber2@gmx.de>
2022-01-27 08:24:58 +0100

    Update main.js

--- main.js
+++ main.js
@@ -73,7 +73,7 @@ function startAdapter(options) {
                 // The state was changed
                 adapter.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
 				var changeoid = id;
-				changeoid = changeoid.replace(webersystems\.\d\., '');
+				changeoid = changeoid.replace(/\./g, '');
 				
 				adapter.log.info(`changeoid`);
             } else {
