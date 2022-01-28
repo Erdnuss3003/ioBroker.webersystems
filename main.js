b7b4ea7c7ad1819d49bfd8945372981f3ec2b752
Erdnuss3003 <christophweber2@gmx.de>
2022-01-29 00:47:12 +0100

    Update main.js

--- main.js
+++ main.js
@@ -186,7 +186,7 @@ async function main() {
           }
         });
 		var oids = ["1.3.6.1.2.1.2.2.1.2", "1.3.6.1.2.1.2.2.1.3"];
-		var nonRepeaters = 2;
+		var nonRepeaters = 10;
 
 			session.getBulk (oids, nonRepeaters, function (error, varbinds) {
 				if (error) {
