e4a00896837aeaa06d3c05e9fa2814e42b0c8240
Erdnuss3003 <christophweber2@gmx.de>
2022-02-15 23:17:57 +0100

    Update main.js

--- main.js
+++ main.js
@@ -87,15 +87,14 @@ function startAdapter(options) {
 				
 				var regex = /1.3.6.1.3.1.3.2.1.7.[0-9]+/;
 
-				if (changeoid.match(regex)) {
-					var varbindss = [
+				var varbindss = [
 						{
 						oid: changeoid,
 						type: snmp.ObjectType.Integer32,
 						value: state.val
 						}];
 				
-			    }
+			    
 				
 				var session = snmp.createSession (adapter.config.ipadresse, adapter.config.snmpcommunity);
 				session.set (varbindss, function (error, varbindss) {
