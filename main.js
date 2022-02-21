7e42105f95aebc22b885d9596302fe0069987ecb
Erdnuss3003 <christophweber2@gmx.de>
2022-02-21 22:09:10 +0100

    Update main.js

--- main.js
+++ main.js
@@ -252,7 +252,7 @@ async function main() {
 		function feedCb (varbinds) {
 			for (var i = 0; i < varbinds.length; i++) {
 				if (snmp.isVarbindError (varbinds[i]))
-					 adapter.log.info (snmp.varbindError (varbinds[i]));
+					 adapter.log.info ('error walk');
 				else
 					adapter.log.info (varbinds[i].oid + "|" + varbinds[i].value);
 					oids = varbinds[i].oid;
