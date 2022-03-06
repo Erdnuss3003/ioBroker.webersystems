e7cbd22e0789933e3124dc7d1a220f7335b1c9c3
Erdnuss3003 <christophweber2@gmx.de>
2022-03-06 22:32:10 +0100

    Update main.js

--- main.js
+++ main.js
@@ -467,7 +467,7 @@ async function poeoids() {
 					adapter.log.info (varbinds[i].oid + "|" + varbinds[i].value);
 					oids = varbinds[i].oid;
 					oids = oids.replace(/\./g, '_');
-					oids = "poe." + "." + oids;
+					oids = "poe." + oids;
 					adapter.setObjectNotExistsAsync(oids, {type: 'state', common: {name: 'pethPsePortAdminEnable', type: 'string', role: 'value', read: true, write: false}, native: {}, });								 
 					adapter.setState(oids, varbinds[i].value.toString(), true);															
 						
