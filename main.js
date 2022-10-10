9b30322e0dd9dc0316890c213f47c8a76a182793
Erdnuss3003 <christophweber2@gmx.de>
2022-10-10 10:53:26 +0200

    Update main.js

--- main.js
+++ main.js
@@ -135,12 +135,28 @@ oid = "1.3.6.1.2.1.2.2.1.1";
 session.subtree (oid, maxRepetitions, feedCb, doneCb);
 	
 }
+async function interfaces_ifdescr() {
 
-async function poeoids() {
+	if (adapter.config.ifdescr) {
+		var oiddescr = "1.3.6.1.2.1.2.2.1.2";
+		var oiddescrvalue = "0";
+		var oiddescrvaluee = "0";
 
+		oiddescrvalue = oiddescr + "." + varbinds[i].value;
+		oiddescrvaluee = oiddescrvalue.replace(/\./g, '_');
+		oiddescrvaluee = "interface." + varbinds[i].value + "." + oiddescrvaluee;
 
-		
+		var oidsifdescr = [oiddescrvalue];
 
+		session.get (oidsifdescr, function (error, varbinds) {
+			if (error) {
+				adapter.log.info('snmp error oidsifdescr ');
+			} else {
+				adapter.setObjectNotExistsAsync(oiddescrvaluee, {type: 'state', common: {name: 'ifDecsr', type: 'string', role: 'value', read: true, write: false}, native: {}, });									
+				adapter.setState(oiddescrvaluee, varbinds[0].value.toString(), true);
+			}
+		}			
+	}
 }
 
 async function dataPolling() {
