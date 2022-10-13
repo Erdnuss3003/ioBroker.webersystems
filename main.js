d2182c81b2629fd2a6ce6132b6c45a1632d633cd
Erdnuss3003 <christophweber2@gmx.de>
2022-10-13 17:03:59 +0200

    Update main.js

--- main.js
+++ main.js
@@ -107,7 +107,8 @@ async function system() {
 				adapter.log.info('snmp error oidssysDescr ');
 			} else {
 				adapter.setObjectNotExistsAsync(oidsysDescrvalue, {type: 'state', common: {name: 'sysDescr', type: 'string', role: 'value', read: true, write: false}, native: {}, });									
-				// adapter.setState(oidsysDescrvalue, varbinds[0].value.toString(), true);
+				sleep(500);
+				adapter.setState(oidsysDescrvalue, varbinds[0].value.toString(), true);
 			}
 		});			
 	}
@@ -125,7 +126,8 @@ async function system() {
 				adapter.log.info('snmp error oidssysUpTime ');
 			} else {
 				adapter.setObjectNotExistsAsync(oidsysUpTimevalue, {type: 'state', common: {name: 'sysUpTime', type: 'string', role: 'value', read: true, write: false}, native: {}, });									
-				//adapter.setState(oidsysUpTimevalue, varbinds[0].value.toString(), true);
+				sleep(500);
+				adapter.setState(oidsysUpTimevalue, varbinds[0].value.toString(), true);
 			}
 		});			
 	}
